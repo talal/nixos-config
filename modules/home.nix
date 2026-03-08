877a0a644d929cdd6f1eb8714ec932088ab2f1ea
Muhammad Talal Anwar <3526562+talal@users.noreply.github.com>
2026-03-08 14:46:08 +0100

    mpv: improve osc

--- modules/home.nix
+++ modules/home.nix
@@ -238,6 +238,9 @@
           hwdec = true;
         };
         modernz = {
+          window_top_bar = false;
+          osc_on_start = true;
+          showonpause = false;
           hover_effect = "color";
           hover_effect_color = "#8aadf4";
           seekbarfg_color = "#8aadf4";
