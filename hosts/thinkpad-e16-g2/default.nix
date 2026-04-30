9b054ad46381c3fc727bccd8bf579e743b3861f3
Muhammad Talal Anwar <3526562+talal@users.noreply.github.com>
2026-04-30 17:57:32 +0200

    feat: use latest kernel

--- hosts/thinkpad-e16-g2/default.nix
+++ hosts/thinkpad-e16-g2/default.nix
@@ -69,7 +69,7 @@
   };
 
   # ══════════ Performance ══════════
-  boot.kernelPackages = pkgs.linuxKernel.packages.linux_6_19;
+  boot.kernelPackages = pkgs.linuxPackages_latest;
 
   # This ensures WiFi, Bluetooth, and CPU Microcode get the binary blobs they need.
   hardware.enableRedistributableFirmware = true;
