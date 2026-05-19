ae7f469f6612bf0c5bf1b63e27a93cc51b86d1d1
Muhammad Talal Anwar <noreply@talal.ch>
2026-05-19 22:58:01 +0200

    git: do not override https URL

--- modules/git.nix
+++ modules/git.nix
@@ -39,7 +39,7 @@
         gpg.format = "ssh";
         commit.gpgSign = true;
         tag.gpgSign = true;
-        url."git@github-talal:".insteadOf = ["git@github.com:" "https://github.com/"];
+        url."git@github-talal:".insteadOf = ["git@github.com:"];
         alias = {
           l = "log --graph --abbrev-commit --pretty='%C(bold yellow)%h%C(reset) %C(green)(%cr)%C(reset)%C(bold red)%d%C(reset) %s %C(blue)<%an>%C(reset)'";
 
