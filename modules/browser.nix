ae1728101342ed18706e1c68693e8160e1f512f2
Muhammad Talal Anwar <noreply@talal.ch>
2026-05-25 18:45:35 +0200

    browser: more privacy for chromium

--- modules/browser.nix
+++ modules/browser.nix
@@ -4,12 +4,23 @@
   ...
 }: let
   langs = ["en-GB-oxendict" "de-DE"];
+  allowedExtensions = [
+    "nngceckbapebfimnlniiiahkandclblb" # Bitwarden
+    "mdjildafknihdffpkfmmpnpoiajfjnjd" # Consent-O-Matic
+    "mnjggcdmjocbbbhaepdhchncahnbgone" # SponsorBlock for YouTube
+    "khncfooichmfjbepaaaebmommgaepoid" # Unhook
+    "cnjifjpddelmedmihgijeibhnjfabmlf" # Obsidian Web Clipper
+    "ghbmnnjooekpmoecnnnilnnbdlolhkhi" # Google Docs Offline
+    "jabopobgcpjmedljpbcaablpmlmfcogm" # WhatFont
+    # Ideally, this should be defined in modules/catppuccin.nix and the list merged but
+    # unfortunately programs.chromium.extraOpts is of type lib.types.attrs 😔
+    "cmpdlhmnmjhihmcfnigoememnffkimlk" # Catppuccin Macchiato theme
+  ];
 in {
   programs.chromium = {
     enable = true;
     extraOpts = {
-      # Brave specific policies
-      # keep-sorted start
+      # keep-sorted start prefix_order=Brave
       BraveAIChatEnabled = false;
       BraveNewsDisabled = true;
       BraveRewardsDisabled = true;
@@ -17,22 +28,6 @@ in {
       BraveTalkDisabled = true;
       BraveVPNDisabled = true;
       BraveWalletDisabled = true;
-      # keep-sorted end
-
-      # Common Chromium policies
-      ExtensionInstallAllowlist = [
-        "nngceckbapebfimnlniiiahkandclblb" # Bitwarden
-        "mdjildafknihdffpkfmmpnpoiajfjnjd" # Consent-O-Matic
-        "mnjggcdmjocbbbhaepdhchncahnbgone" # SponsorBlock for YouTube
-        "khncfooichmfjbepaaaebmommgaepoid" # Unhook
-        "cnjifjpddelmedmihgijeibhnjfabmlf" # Obsidian Web Clipper
-        "ghbmnnjooekpmoecnnnilnnbdlolhkhi" # Google Docs Offline
-        "jabopobgcpjmedljpbcaablpmlmfcogm" # WhatFont
-        # Ideally, this should be defined in modules/catppuccin.nix and the list merged but
-        # unfortunately programs.chromium.extraOpts is of type lib.types.attrs 😔
-        "cmpdlhmnmjhihmcfnigoememnffkimlk" # Catppuccin Macchiato theme
-      ];
-      # keep-sorted start
       AutofillAddressEnabled = false;
       AutofillCreditCardEnabled = false;
       AutoplayAllowed = false;
@@ -41,19 +36,27 @@ in {
       BrowserLabsEnabled = false;
       BrowserSignin = 0;
       DefaultBrowserSettingEnabled = false;
+      ExtensionInstallAllowlist = allowedExtensions;
       ExtensionInstallBlocklist = ["*"];
+      FeedbackSurveysEnabled = false;
       ForcedLanguages = langs ++ ["ur"];
       HomepageIsNewTabPage = true;
       HttpsOnlyMode = "force_enabled";
+      MediaRecommendationsEnabled = false;
+      MetricsReportingEnabled = false;
+      PasswordLeakDetectionEnabled = false;
       PasswordManagerEnabled = false;
       PasswordManagerPasskeysEnabled = false;
+      PasswordSharingEnabled = false;
       PrivacySandboxAdMeasurementEnabled = false;
       PrivacySandboxAdTopicsEnabled = false;
+      PrivacySandboxFingerprintingProtectionEnabled = true;
       PrivacySandboxPromptEnabled = false;
       PrivacySandboxSiteEnabledAdsEnabled = false;
       PromotionsEnabled = false;
       RestoreOnStartup = 1; # restore last session
       SearchSuggestEnabled = false;
+      ShoppingListEnabled = false;
       ShowHomeButton = false;
       SpellcheckEnabled = true;
       SpellcheckLanguage = langs;
