8ed060ec24286bb0a55aacf34c95e3cd21e85591
Muhammad Talal Anwar <noreply@talal.ch>
2026-05-26 22:34:51 +0200

    refactor: clean up

---
+++ modules/mpv.nix
@@ -0,0 +1,68 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
+  home-manager.users.${config.user} = {
+    programs.mpv = {
+      enable = true;
+      config = {
+        profile = "gpu-hq";
+        gpu-api = "vulkan";
+        gpu-context = "waylandvk";
+        hwdec = "auto";
+        vo = "gpu-next"; # https://github.com/mpv-player/mpv/wiki/GPU-Next-vs-GPU
+        ao = "pipewire";
+
+        border = false;
+        osc = false;
+        osd-bar = false;
+
+        hr-seek = true;
+        keep-open = true;
+        reset-on-next-file = "video-zoom,panscan,video-unscaled,video-rotate,video-align-x,video-align-y";
+        save-position-on-quit = true;
+        save-watch-history = true;
+        volume = 20;
+
+        slang = "en,eng,de,deu,ger";
+        alang = "ja,jp,jpn,de,deu,ger,en,eng"; # Onii-Chan!
+
+        ytdl-format = "bestvideo[height<=?1080]+bestaudio/best";
+        screenshot-dir = "~/Pictures/Screenshots/mpv";
+      };
+      scripts = with pkgs.mpvScripts; [modernz thumbfast mpris sponsorblock];
+      scriptOpts = {
+        thumbfast = {
+          spawn_first = true;
+          network = true;
+          hwdec = true;
+        };
+        modernz = {
+          osc_on_seek = false;
+          osc_on_start = true;
+          showonpause = false;
+          window_top_bar = false;
+
+          hover_effect = "color";
+          hover_effect_color = "#8aadf4";
+          seekbarbg_color = "#1e2030";
+          seekbarfg_color = "#8aadf4";
+        };
+        ytdl_hook = {
+          ytdl_path = "${lib.getExe pkgs.unstable.yt-dlp}";
+        };
+      };
+      bindings = {
+        UP = "add volume 2";
+        DOWN = "add volume -2";
+        "Alt+LEFT" = "seek -60";
+        "Alt+RIGHT" = "seek 60";
+        V = "cycle secondary-sub-visibility";
+        "Ctrl+j" = "cycle secondary-sid";
+        "Ctrl+J" = "cycle secondary-sid down";
+      };
+    };
+  };
+}
--- profiles/desktop.nix
+++ profiles/desktop.nix
@@ -24,6 +24,7 @@ in {
     ../modules/git.nix
     ../modules/helix.nix
     ../modules/jj.nix
+    ../modules/mpv.nix
     ../modules/nextdns.nix
     ../modules/podman.nix
     ../modules/printing.nix
@@ -225,66 +226,6 @@ in {
           alpha = 0.90;
           # blur = true; # TODO: enable when foot is updated to latest version
         };
-      };
-    };
-
-    programs.mpv = {
-      enable = true;
-      config = {
-        profile = "gpu-hq";
-        gpu-api = "vulkan";
-        gpu-context = "waylandvk";
-        hwdec = "auto";
-        vo = "gpu-next"; # https://github.com/mpv-player/mpv/wiki/GPU-Next-vs-GPU
-        ao = "pipewire";
-
-        border = false;
-        osc = false;
-        osd-bar = false;
-
-        hr-seek = true;
-        keep-open = true;
-        reset-on-next-file = "video-zoom,panscan,video-unscaled,video-rotate,video-align-x,video-align-y";
-        save-position-on-quit = true;
-        save-watch-history = true;
-        volume = 20;
-
-        slang = "en,eng,de,deu,ger";
-        alang = "ja,jp,jpn,de,deu,ger,en,eng"; # Onii-Chan!
-
-        ytdl-format = "bestvideo[height<=?1080]+bestaudio/best";
-        screenshot-dir = "~/Pictures/Screenshots/mpv";
-      };
-      scripts = with pkgs.mpvScripts; [modernz thumbfast mpris sponsorblock];
-      scriptOpts = {
-        thumbfast = {
-          spawn_first = true;
-          network = true;
-          hwdec = true;
-        };
-        modernz = {
-          osc_on_seek = false;
-          osc_on_start = true;
-          showonpause = false;
-          window_top_bar = false;
-
-          hover_effect = "color";
-          hover_effect_color = "#8aadf4";
-          seekbarbg_color = "#1e2030";
-          seekbarfg_color = "#8aadf4";
-        };
-        ytdl_hook = {
-          ytdl_path = "${lib.getExe pkgs.unstable.yt-dlp}";
-        };
-      };
-      bindings = {
-        UP = "add volume 2";
-        DOWN = "add volume -2";
-        "Alt+LEFT" = "seek -60";
-        "Alt+RIGHT" = "seek 60";
-        V = "cycle secondary-sub-visibility";
-        "Ctrl+j" = "cycle secondary-sid";
-        "Ctrl+J" = "cycle secondary-sid down";
       };
     };
 
