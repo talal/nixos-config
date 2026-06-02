7264e0380c0ac482090f100fdde6fb129f350b00
Muhammad Talal Anwar <noreply@talal.ch>
2026-06-02 15:52:17 +0200

    syncthing: don't create default `~/Sync` folder

--- modules/profiles/syncthing.nix
+++ modules/profiles/syncthing.nix
@@ -7,4 +7,7 @@
     # https://docs.syncthing.net/users/config.html#configuration-file-locations
     configDir = "/home/${config.user}/.local/state/syncthing"; # folder for syncthing settings and keys
   };
+
+  # Don't create default ~/Sync folder.
+  systemd.services.syncthing.environment.STNODEFAULTFOLDER = "true";
 }
