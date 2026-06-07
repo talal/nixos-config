40665a95f2b1c3243ad45d50e6ae714e25360bb0
Muhammad Talal Anwar <noreply@talal.ch>
2026-06-07 19:32:01 +0200

    kanata: add symbols and numbers layer

--- modules/profiles/kanata.nix
+++ modules/profiles/kanata.nix
@@ -31,33 +31,40 @@ in {
           esc
           tab  q w e r t y u i o p [ ]
           caps a s d f g h j k l ; ' \
-                         n m , .
-          102d         spc
+               z x c v b n m , . /
+          102d lalt      spc
         )
-
 
         (deflayer default
           caps
           @tabmeh _  _  _  _  _ _ _  @i _  _  _ _
           @escext @a @s @d @f _ _ @j @k @l @; _ _
-                                _ _  _  _
-          grv               @spcnum
+                  _  _  _  _  _ _ _  _  _  _
+          grv     @num       @sym
         )
 
         (deflayer extend
           _
           _  _    _    _    _    _  _    pgdn pgup _    _    _   _
           _  lmet lalt lsft lctl _  left down up   rght bspc del _
-                                    _    _    home end
-          _                      enter
+             _    _    _    _    _  _    _    home end  _
+          _  _                   enter
         )
 
         (deflayer number
           _
-          _  S-1 S-2 S-3 S-4 S-5 S-6 S-7 S-8 S-9 S-0 S-- S-=
-          _  1   2   3   4   5   6   7   8   9   0   -   =
-                                 _   _   _   _
-          _                    _
+          _  _ _ _ _ _ _ 7 8 9 _ _ _
+          _  _ _ _ _ _ _ 4 5 6 _ _ _
+             _ _ _ _ _ _ 1 2 3 _
+          _  _        0
+        )
+
+        (deflayer symbols
+          _
+          _  @! @@   @#   @$ @%    @^  @&  @* _  _  _ _
+          _  @{ lpar rpar @} @:    eql min @[ @] @+ _ _
+             _  _    _    _  grv   @~  _   _  _  _
+          _  _                   _
         )
 
         (defvar
@@ -71,24 +78,44 @@ in {
         (defalias
           ;; ══════════ Layers ══════════
           escext (tap-hold-press $tt $ht esc (layer-while-held extend))
-          spcnum (tap-hold-press $tt $ht spc (layer-while-held number))
+          num    (tap-hold-press $tt $ht lalt (layer-while-held number))
+          sym    (tap-hold-release $tt $ht spc (layer-while-held symbols))
 
-          ;; ══════════ Mod Keys ══════════
-          ;; spchyp (tap-hold-press $tt $ht spc (multi lmet lctl lalt lsft))
+          ;; ══════════ Home Row Mods ══════════
           tabmeh (tap-hold-press $tt $ht tab (multi lctl lalt lsft))
 
-          ;; Home Row Mod
-          a (tap-hold $tt $ht a lmet)
-          s (tap-hold $tt $ht s lalt)
-          d (tap-hold $tt $ht d lsft)
-          f (tap-hold $tt $ht f lctl)
-          j (tap-hold $tt $ht j rctl)
-          k (tap-hold $tt $ht k rsft)
-          l (tap-hold $tt $ht l lalt)
-          ; (tap-hold $tt $ht ; lmet)
+          a (tap-hold-release $ht $ht a lmet)
+          s (tap-hold-release $ht $ht s lalt)
+          d (tap-hold-release $ht $ht d lsft)
+          f (tap-hold-release $ht $ht f lctl)
 
-          ;; i is not on home row but close enough...
-          i (tap-hold $tt $ht i ralt)
+          j (tap-hold-release $ht $ht j rctl)
+          k (tap-hold-release $ht $ht k rsft)
+          l (tap-hold-release $ht $ht l lalt)
+          ; (tap-hold-release $ht $ht ; lmet)
+
+          i (tap-hold-press $ht $ht i ralt)
+
+          ;; ══════════ Symbol Aliases ══════════
+          !  S-1
+          @  S-2
+          #  S-3
+          $  S-4
+          %  S-5
+          ^  S-6
+          &  S-7
+          *  S-8
+          _  S-min
+          +  S-eql
+          |  S-bksl
+          ~  S-grv
+          :  S-;
+          [  [
+          ]  ]
+          {  S-[
+          }  S-]
+          <  S-,
+          >  S-.
         )
       '';
     };
