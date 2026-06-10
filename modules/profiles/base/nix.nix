f318df27237d6588d7a60c22f9333dc4f7e48106
Muhammad Talal Anwar <noreply@talal.ch>
2026-06-11 01:44:09 +0200

    base: extract out nix as its own module

---
+++ modules/profiles/base/nix.nix
@@ -0,0 +1,61 @@
+{inputs, ...}: {
+  nixpkgs.config = {
+    allowAliases = false;
+    allowUnfree = true;
+    permittedInsecurePackages = [
+      "electron-39.8.10" # EOL but still used by bitwarden-desktop
+    ];
+  };
+
+  nix = {
+    # Disable legacy channels since we use flake.
+    channel.enable = false;
+
+    # Map the "nixpkgs" identifier to our locked flake inputs.
+    # This prevents downloading nixpkgs again for shell commands or rebuilds.
+    registry = {
+      nixpkgs.flake = inputs.nixpkgs;
+      nixpkgs-unstable.flake = inputs.nixpkgs-unstable;
+    };
+
+    # Set legacy nixpkgs path to flake reference.
+    # Adds the path to NIX_PATH for legacy tool compatibility.
+    nixPath = ["nixpkgs=${inputs.nixpkgs}"];
+
+    # Cleanup
+    optimise.automatic = true;
+    gc = {
+      automatic = true;
+      options = "--delete-older-than 7d";
+    };
+
+    settings = {
+      allowed-users = ["@wheel"]; # can do anything with the nix daemon
+      trusted-users = ["@wheel"]; # can manage the nix store
+
+      experimental-features = [
+        "nix-command"
+        "flakes"
+        "auto-allocate-uids"
+        "cgroups"
+      ];
+      auto-allocate-uids = true;
+      use-cgroups = true;
+
+      # allow-import-from-derivation = false; # TODO: this breaks devenv
+      accept-flake-config = false; # don't accept other people's nix config
+      http-connections = 50; # (default: 25)
+      keep-going = true; #  continue building derivations even if one fails
+      log-lines = 30; # (default: 10)
+      warn-dirty = false;
+
+      # for direnv GC roots
+      keep-derivations = true;
+      keep-outputs = true;
+
+      # cache.nixos.org is included by default.
+      substituters = ["https://talal.cachix.org"];
+      trusted-public-keys = ["talal.cachix.org-1:BHrhoYM/V2GMjBW7aqgQEt1KcPOebRBtq8wUagUwDiw="];
+    };
+  };
+}
