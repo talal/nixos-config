67972929fe5f85b341116b374cd2d6cef11d45b5
Muhammad Talal Anwar <noreply@talal.ch>
2026-06-23 01:06:25 +0200

    chore: clean up

--- modules/profiles/dev/hunk.nix
+++ modules/profiles/dev/hunk.nix
@@ -8,7 +8,7 @@
       inputs.hunk.packages.${pkgs.stdenv.hostPlatform.system}.hunk
     ];
 
-    xdg.configFile."hunk/config.toml".source = (pkgs.formats.toml {}).generate "hunk-config" {
+    xdg.configFile."hunk/config.toml".source = (pkgs.formats.toml {}).generate "hunk-config.toml" {
       theme = "catppuccin-mocha";
       mode = "auto";
       line_numbers = true;
