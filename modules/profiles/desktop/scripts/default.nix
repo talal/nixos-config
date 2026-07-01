e9c438f2cb319b96971cf7362245ffb171058d09
Muhammad Talal Anwar <noreply@talal.ch>
2026-07-01 16:57:01 +0200

    scripts: install as user packages

--- modules/profiles/desktop/scripts/default.nix
+++ modules/profiles/desktop/scripts/default.nix
@@ -1,23 +1,25 @@
-{pkgs, ...}: {
-  hm = {
-    home.packages = with pkgs; [
-      # keep-sorted start block=yes newline_separated=yes
-      (writeShellScriptBin "doctor" (builtins.readFile ./doctor.bash))
+{
+  config,
+  pkgs,
+  ...
+}: {
+  users.users.${config.user}.packages = with pkgs; [
+    # keep-sorted start block=yes newline_separated=yes
+    (writeShellScriptBin "doctor" (builtins.readFile ./doctor.bash))
 
-      (writeShellScriptBin "flatpak-update" ''
-        flatpak update -y
-        flatpak uninstall -y --unused
-      '')
+    (writeShellScriptBin "flatpak-update" ''
+      flatpak update -y
+      flatpak uninstall -y --unused
+    '')
 
-      (writeShellScriptBin "jsonsort" ''
-        set -euo pipefail
-        tmpfile=$(mktemp --suffix=.json)
-        jq -S . "$1" >"$tmpfile"
-        mv "$tmpfile" "$1"
-      '')
+    (writeShellScriptBin "jsonsort" ''
+      set -euo pipefail
+      tmpfile=$(mktemp --suffix=.json)
+      jq -S . "$1" >"$tmpfile"
+      mv "$tmpfile" "$1"
+    '')
 
-      (writeShellScriptBin "set-battery-thresholds" (builtins.readFile ./set-battery-thresholds.bash))
-      # keep-sorted end
-    ];
-  };
+    (writeShellScriptBin "set-battery-thresholds" (builtins.readFile ./set-battery-thresholds.bash))
+    # keep-sorted end
+  ];
 }
