794126e26dee35fdc7b053efe9618229d3f2e6e4
Muhammad Talal Anwar <noreply@talal.ch>
2026-07-24 15:09:08 +0200

    noctalia: update

--- flake.nix
+++ flake.nix
@@ -21,7 +21,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    noctalia.url = "github:noctalia-dev/noctalia/v5.0.0-beta.3";
+    noctalia.url = "github:noctalia-dev/noctalia/v5.0.0-beta.4";
 
     talal-fonts = {
       url = "github:talal/fonts";
