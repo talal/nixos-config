846b65f94838d52f719821bb2fcfe1bde9ad46d8
Muhammad Talal Anwar <noreply@talal.io>
2026-08-05 23:12:21 +0200

    nixpkgs: remove electron 39 override

--- modules/profiles/core/nixpkgs.nix
+++ modules/profiles/core/nixpkgs.nix
@@ -2,9 +2,5 @@
   nixpkgs.config = {
     allowAliases = false;
     allowUnfree = true;
-    permittedInsecurePackages = [
-      # TODO: remove when bitwarden-deskop upgrades its electron version.
-      "electron-39.8.10"
-    ];
   };
 }
