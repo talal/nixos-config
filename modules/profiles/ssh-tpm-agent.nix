6686db42be8315317283fae3dc939730ee96baf4
Muhammad Talal Anwar <noreply@talal.io>
2026-08-19 22:36:17 +0200

    ssh-tpm-agent: add fallback agent for non-TPM keys

--- modules/profiles/ssh-tpm-agent.nix
+++ modules/profiles/ssh-tpm-agent.nix
@@ -10,12 +10,12 @@
   services.gnome.gcr-ssh-agent.enable = lib.mkForce false;
 
   hm = {
-    services.ssh-agent.enable = lib.mkForce false;
+    services.ssh-agent.enable = true;
     services.ssh-tpm-agent.enable = true;
 
     systemd.user.services.ssh-tpm-agent-init = {
       Unit = {
-        Description = "Load ssh-tpm-agent keys";
+        Description = "Load ssh-tpm-agent keys on startup";
         After = ["ssh-tpm-agent.service"];
         Requires = ["ssh-tpm-agent.service"];
       };
