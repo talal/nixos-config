86090a6dcf2aa2860ddb0693ea22629c18f9405b
Muhammad Talal Anwar <noreply@talal.io>
2026-08-20 05:20:17 +0200

    desktop: install logseq nightly

--- modules/profiles/desktop/packages.nix
+++ modules/profiles/desktop/packages.nix
@@ -13,6 +13,7 @@
     ])
     ++ (with pkgs; [
       # keep-sorted start
+      (callPackage ../../../pkgs/logseq-bin.nix {})
       adw-gtk3 # GTK theme
       adwaita-icon-theme
       apple-cursor
