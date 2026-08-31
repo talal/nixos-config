5316dc93e9efb6fea90b786aa37168f632d53241
Muhammad Talal Anwar <noreply@talal.io>
2026-08-31 04:17:20 +0200

    Install latest kernel

--- hosts/thinkpad/default.nix
+++ hosts/thinkpad/default.nix
@@ -1,5 +1,4 @@
 {
-  lib,
   myModules,
   pkgs,
   ...
@@ -58,16 +57,17 @@
     ];
   };
 
-  boot.kernelPackages = pkgs.linuxPackagesFor (pkgs.linuxKernel.kernels.linux_7_1.override {
-    argsOverride = rec {
-      src = pkgs.fetchurl {
-        url = "mirror://kernel/linux/kernel/v${lib.versions.major version}.x/linux-${version}.tar.xz";
-        sha256 = "sha256-IqAZazy83zTcJ7d1YfTQQFhf00R+3JqzUxoax54wQec=";
-      };
-      version = "7.1.5";
-      modDirVersion = "7.1.5";
-    };
-  });
+  # boot.kernelPackages = pkgs.linuxPackagesFor (pkgs.linuxKernel.kernels.linux_7_1.override {
+  #   argsOverride = rec {
+  #     src = pkgs.fetchurl {
+  #       url = "mirror://kernel/linux/kernel/v${lib.versions.major version}.x/linux-${version}.tar.xz";
+  #       sha256 = "sha256-IqAZazy83zTcJ7d1YfTQQFhf00R+3JqzUxoax54wQec=";
+  #     };
+  #     version = "7.1.5";
+  #     modDirVersion = "7.1.5";
+  #   };
+  # });
+  boot.kernelPackages = pkgs.linuxPackages_latest;
 
   # ══════════ Performance ══════════
   # This ensures WiFi, Bluetooth, and CPU Microcode get the binary blobs they need.
